1ab9dc97e556b7c2ebf8822505069b2f4e344add
Anjali Singh <anjalikumari98.com@gmail.com>
2026-09-19 16:58:35 +0530

    Delete frontend directory

--- frontend/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Navbar";
-export * from "./Sidebar";
-export * from "./Footer";
-export * from "./Container";
-export * from "./PageHeader";
--- frontend/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const APP_NAME = "Tech Tians Academy";
-
-export const ROUTES = {} as const;
